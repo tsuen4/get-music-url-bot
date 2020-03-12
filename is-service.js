a8bb7db4c9503a21f92bb18f3d46c581b9691c40
tsuen4 <ht17a045@oecu.jp>
2020-03-12 23:57:23 +0900

    Firebase Realtime Databse にデータを登録するように

--- is-service.js
+++ is-service.js
@@ -1,3 +1,5 @@
+const firebase = require('./firebase')
+
 const getYouTubeInfo = require('./get-youtube-info')
 const getSoundCloudInfo = require('./get-soundcloud-info')
 
@@ -6,35 +8,34 @@ const regSoundCloud = /(https:\/\/soundcloud\.com\/.+)/
 
 const reqYouTube = {
   base: 'https://www.googleapis.com/youtube/v3/videos?id=',
-  id: '',
-  key: youtubeAPIKey,
   fields: 'items(snippet(title,channelTitle))',
   part: 'snippet'
 }
 
 const is = {
-  YouTube (msg) {
+  async YouTube (msg, key) {
     for (let el of regYouTube) {
       const matchYouTube = msg.content.match(el)
       if (matchYouTube) {
         const id = matchYouTube[1]
-        const reqUrl = `${reqYouTube.base}${id}&key=${reqYouTube.key}&fields=${reqYouTube.fields}&part=${reqYouTube.part}`
+        const reqUrl = `${reqYouTube.base}${id}&key=${key}&fields=${reqYouTube.fields}&part=${reqYouTube.part}`
 
-        const youtubeUrl = `https://youtu.be/${id}`
+        const YouTubeUrl = `https://youtu.be/${id}`
         const getDataTmp = await getYouTubeInfo(reqUrl, 'items', 'youtube')
         const getData = {
           channel: getDataTmp[0].snippet.channelTitle,
           title: getDataTmp[0].snippet.title
         }
+        firebase.submit(YouTubeUrl, getData, msg.guild.id, 'youtube')
       }
     }
   },
-  SoundCloud (msg) {
+  async SoundCloud (msg) {
     const matchSoundCloud = msg.content.match(regSoundCloud)
     if (matchSoundCloud) {
       const SoundCloudUrl = matchSoundCloud[1]
       const getData = await getSoundCloudInfo(SoundCloudUrl)
-      console.log(SoundCloudUrl, getData)
+      firebase.submit(SoundCloudUrl, getData, msg.guild.id, 'soundcloud')
     }
   }
 }
